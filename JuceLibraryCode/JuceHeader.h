6f52cef9df069425ec3b03d2489ffee39b125e46
May Tiger <dogomen11@gmail.com>
2025-04-03 15:15:25 +0900

    removed juce::animation

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -13,7 +13,6 @@
 #pragma once
 
 
-#include <juce_animation/juce_animation.h>
 #include <juce_audio_basics/juce_audio_basics.h>
 #include <juce_audio_devices/juce_audio_devices.h>
 #include <juce_audio_formats/juce_audio_formats.h>
